e4313aeb4b0486b10d8626b811da57ea0c7fe1b2
NesTerLoN392 <tcncarlos392@gmail.com>
2023-01-31 12:40:48 -0500

    second commit

--- src/pages/ProductsId.jsx
+++ src/pages/ProductsId.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Col, ListGroup, Row } from 'react-bootstrap';
+import { Card, Col, ListGroup, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { productFilterCategory } from '../store/slices/products.slice';
@@ -38,60 +38,65 @@ const ProductsId = () => {
 
     return (
         <div>
+            <div className='buttom-home' onClick={() => navigate(`/`)}>
+                <button className='buttom-home'> Home</button> <div className='container-punto'></div> <h3 className='container-title-padding-right'>{product.title}</h3>
+            </div>
+            <div className='container-superior'>
 
+                <Col className='container-image' lg={6}>
+                    <img className='product-id-img' src={product.images?.[0].url} alt="samsung" />
+
+                </Col>
+                <Col className='container-data' lg={6}>
+                    <h3> {product.brand}</h3>
+                    <h3>{product.title}</h3>
+
+                    <div className='container-quantity'>
+                        <section>
+                            price
+                            <h3>$ {product.price}</h3>
+                        </section>
+                        <section>
+                            Quantity
+                            <div><button onClick={decrement} disabled={counter === 1}>-</button>{counter} <button onClick={increment}>+</button></div>
+                        </section>
+
+                    </div>
+                    <button className='addToCar'>Add Cart <i className='bx bx-cart-add'></i></button>
+                    <h3 className='container-descripcion'> description: {product.description}</h3>
+                </Col>
+            </div>
             <Row>
-                <div className='container-superior'>
-                    <Col className='container-image' lg={7}>
-                        <img className='product-id-img' src={product.images?.[0].url} alt="samsung" />
 
-                    </Col>
-                    <Col className='container-data' lg={5}>
-                        <h3> {product.brand}</h3>
-                        <h3>{product.title}</h3>
+                <Col lg={9}>
+                    <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+                        {productsCategories.map((category) => (
+                            <Col key={category.id}>
+                                <Card key={category.id} onClick={() => navigate(`/products/${category.id}`)}>
+                                    <Card.Img className="g-3"
+                                        variant="top"
+                                        style={{ height: 200, objectFit: "contain", paddingTop: "1rem" }}
+                                        src={category.images?.[0].url} alt="producto" />
 
-                        <div className='container-quantity'>
-                            <section>
-                                price
-                                <h3>$ {product.price}</h3>
-                            </section>
-                            <section>
-                                Quantity
-                                <div><button onClick={decrement} disabled={counter === 1}>-</button>{counter} <button onClick={increment}>+</button></div>
-                            </section>
-
-                        </div>
-                        <button className='addToCar'>Add Cart <i className='bx bx-cart-add'></i></button>
-                        <h3 className='container-descripcion'> description: {product.description}</h3>
-                    </Col>
-                </div>
-                <div className='container-flex'>
-                    <ListGroup as="ol" numbered
-                        >
-                    {productsCategories.map((category) => (
-                            <div className='similar-content'  key={category.id} onClick={() => navigate(`/products/${category.id}`)}>
-                                <div className='container-image-similar'>
-                                    <ListGroup.Item as="li"><img className='img-fluid' src={category.images[0].url} alt="product" /></ListGroup.Item>
-                                </div>
-                                <div>
-                                    <ListGroup.Item as="li">{category.brand}</ListGroup.Item>
-                                    <ListGroup.Item as="li">{category.title}</ListGroup.Item>
-                                    <div className='container-car-prices'>
-                                        <div>
-                                             
-                                            <ListGroup.Item as="li"> Price {category.price}</ListGroup.Item>
+                                    <Card.Body className='card-body'>
+                                        <Card.Title>{category.brand}</Card.Title>
+                                        <Card.Title>{category.title}</Card.Title>
+                                        <div className='container-car-price'>
+                                            <div>
+                                                <Card.Text>
+                                                    Price: $ {category.price}
+                                                </Card.Text>
+                                            </div>
+                                            <div className='car-container'>
+                                                <i className='bx bx-cart-add'></i>
+                                            </div>
                                         </div>
-                                        <div className='car-container-similar'>
-                                            <i className='bx bx-cart-add'></i>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-))}
-</ListGroup>
-                </div>
-
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Col >
             </Row>
         </div>
     );
